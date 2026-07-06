6762bfbd3dedd5211cf2e8f0583bcf7c1014cac5
hesham <atsh.phone@gmail.com>
2026-07-06 10:26:41 +0300

    chore: support older Android versions from Android 5.0 Lollipop (minSdk 21) and bump to v1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,10 @@ android {
 
     defaultConfig {
         applicationId = "com.hwnix.smsagent"
-        minSdk = 26
+        minSdk = 21
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
