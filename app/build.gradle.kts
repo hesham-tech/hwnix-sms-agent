e6821408aa259343796fb65639f2df188eadd7d8
hesham <atsh.phone@gmail.com>
2026-07-31 03:10:23 +0300

    chore(release): release android sms agent v1.0.69 with limit alerts & network stability fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.hwnix.cash"
         minSdk = 26
         targetSdk = 34
-        versionCode = 68
-        versionName = "1.0.68"
+        versionCode = 69
+        versionName = "1.0.69"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
